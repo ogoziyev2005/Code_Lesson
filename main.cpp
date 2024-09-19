0e02be5fff4d46b6c077586c7932d6072fe145c6
ogoziyev2005 <ogoziyev2005@gmail.com>
2024-09-19 04:09:18 -0700

    commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n;
+    cin>>n;
+    switch(n) {
+        case 1:
+            cout<<"One\n";
+        break;
+        case 2:
+            cout<<"Two\n";
+        break;
+        case 3:
+            cout<<"Three\n";
+        break;
+    }
+    return 0;
+}
